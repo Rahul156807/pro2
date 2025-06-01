988726332c4de75e77e195479e2f8191da96e183
Rahul156807 <boss123@gmail.com>
2025-06-01 17:40:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express"); 
 const bodyParser = require("body-parser");
-
+const port =  process.env.PORT || 3000;
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const mongoose = require("mongoose");
+// mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/todoDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
 mongoose.connect("mongodb://localhost:27017/todo");
 
 const trySchema = new mongoose.Schema({
